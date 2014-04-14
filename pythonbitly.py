b33f2673640d804b2f2c4a7aec9ca6f8d3430e1a
insanesyntax <tomwolfskaempf@gmail.com>
2014-04-14 18:43:04 +0200

    Added the code

---
+++ pythonbitly.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  get_bitly.py
+#  
+#  Copyright 2014 tom <tom@ubuntu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+#  
+#  
+
+import requests, json, pyperclip
+
+
+class bcolors:
+    #Diese Klasse definiert verschidene Farben für den Terminaloutput
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+        self.BOLD = ''
+
+def get_short(longURL):
+    #Die Hauptsächliche Funktion dieses Skripts.
+    #Sie nimmt eine lange URL entgegen, wandelt diese in Kurzform um, gibt diese zurück und kopiert sie in die Zwischenablage. 
+    query_params = {
+    'access_token': 'YOUR_TOKEN_HERE',
+    'longUrl': longURL
+    }
+    endpoint = 'https://api-ssl.bitly.com/v3/shorten'
+    response = requests.get(endpoint, params=query_params, verify=False)
+    data = json.loads(response.content)
+    shortURL = data['data']['url']
+    pyperclip.copy(shortURL)
+    return shortURL
+
+longURL = raw_input("Dein langer Link: ")
+
+print "Dein gekürzter Link: ", bcolors.HEADER, bcolors.BOLD, get_short(longURL), bcolors.ENDC
+print "Er befindet sich auch in der Zwischenablage."
